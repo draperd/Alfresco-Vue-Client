0b840d93f7ffca09d75615515f1cd34ab572990e
ddraper <david.draper@alfresco.com>
2017-01-12 16:19:42 +0000

    Change login button label

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -19,7 +19,7 @@
           v-model="password"
         >
       </div>
-      <button class="btn btn-primary" @click="submit()">Access</button>
+      <button class="btn btn-primary" @click="submit()">Login</button>
    </div>
 </template>
 
